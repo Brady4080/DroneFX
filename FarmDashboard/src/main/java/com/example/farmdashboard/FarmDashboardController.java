8a1ae555f7bd163008dbd7003ee4a9621a3373f8
Brady <bradyp4080@gmail.com>
2023-10-19 23:25:55 -0500

    changed all the border panes to just Panes, gave them all labels and red borders, also imported the drone image

--- FarmDashboard/src/main/java/com/example/farmdashboard/FarmDashboardController.java
+++ FarmDashboard/src/main/java/com/example/farmdashboard/FarmDashboardController.java
@@ -6,7 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -14,22 +14,35 @@ import java.util.ResourceBundle;
 
 public class FarmDashboardController implements Initializable {
     @FXML
-    private BorderPane borderPane;
-
+    private Pane barn;
     @FXML
-    private TreeView tree_view;
-
+    private Pane cattle;
+    @FXML
+    private Pane drone_pane;
+    @FXML
+    private Pane command_center;
+    @FXML
+    private TreeView<String> tree_view;
     @FXML
     private Button add_container;
 
     @Override public void initialize(URL arg0, ResourceBundle arg1){
         // how to change the border color: https://docs.oracle.com/javafx/2/api/javafx/scene/doc-files/cssref.html#border
-        borderPane.setStyle("-fx-border-color: red;");
+        barn.setStyle("-fx-border-color: red;");
+        cattle.setStyle("-fx-border-color: red;");
+        drone_pane.setStyle("-fx-border-color: red;");
+        command_center.setStyle("-fx-border-color: red;");
 
-        // setting the text for the Barn Pane
+        // setting labels for the panes
         Label label = new Label("Barn");
-        label.setAlignment(Pos.TOP_LEFT);
-        borderPane.setTop(label);
+        barn.getChildren().add(label);
+        Label label1 = new Label("Cattle");
+        cattle.getChildren().add(label1);
+        Label label2 = new Label("Drone");
+        drone_pane.getChildren().add(label2);
+        Label label3 = new Label("Command Center");
+        command_center.getChildren().add(label3);
+
 
         // TreeView Pane
         // how to get use the TreeView: https://www.youtube.com/watch?v=CNLHTrY3Nh8&ab_channel=BroCode
