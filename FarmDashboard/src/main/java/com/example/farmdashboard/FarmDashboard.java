55c8477fd0dd566fbc46c560fcaac5968059264f
Connor Guess <137028560+Guess2576@users.noreply.github.com>
2023-10-23 17:31:42 -0500

    Added singleton instance

--- FarmDashboard/src/main/java/com/example/farmdashboard/FarmDashboard.java
+++ FarmDashboard/src/main/java/com/example/farmdashboard/FarmDashboard.java
@@ -1,6 +1,7 @@
 package com.example.farmdashboard;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,8 +9,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class FarmDashboard extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
+
+    private static FarmDashboard instance; // Singleton instance
+
+    private FarmDashboard() {
+    }
+
+    public static FarmDashboard getInstance() {
+        if (instance == null) {
+            instance = new FarmDashboard();
+        }
+        return instance;
+    }
+
+    public void initialize(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FarmDashboard.class.getResource("farm-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
         stage.setTitle("Farm Dashboard");
@@ -17,7 +30,19 @@ public class FarmDashboard extends Application {
         stage.show();
     }
 
+    @Override
+    public void start(Stage stage) {
+    }
+
     public static void main(String[] args) {
-        launch();
+        FarmDashboard dashboard = FarmDashboard.getInstance();
+        Platform.runLater(() -> {
+            Stage stage = new Stage();
+            try {
+                dashboard.initialize(stage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
