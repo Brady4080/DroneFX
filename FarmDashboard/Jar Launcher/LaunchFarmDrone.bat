780865f89d3147920b36e96899be4b279eafeb39
Brady <bradyp4080@gmail.com>
2023-11-05 12:39:38 -0600

    fixed the bat

--- FarmDashboard/Jar Launcher/LaunchFarmDrone.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java --module-path ..\..\lib\javafx-sdk-20.0.2/lib --add-modules javafx.controls,javafx.fxml -jar FarmDashboard.jar
---
+++ LaunchFarmDrone.bat
@@ -0,0 +1,2 @@
+@echo off
+java --module-path lib\javafx-sdk-20.0.2/lib --add-modules javafx.controls,javafx.fxml -jar FarmDashboard\JarLauncher\FarmDashboard.jar
