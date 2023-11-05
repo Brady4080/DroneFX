92552bf3ea5d04b8e7af3ab7ec4b07f0f640014f
Brady <bradyp4080@gmail.com>
2023-11-05 17:08:44 -0600

    changed scan logic

--- LaunchFarmDrone.bat
+++ LaunchFarmDrone.bat
@@ -1,2 +1,2 @@
 @echo off
-java --module-path lib\javafx-sdk-20.0.2/lib;lib\javacv\1.5.7 --add-modules javafx.controls,javafx.fxml -jar FarmDashboard\JarLauncher\FarmDashboard.jar
+java --module-path lib\javafx-sdk-20.0.2\lib;lib\javacv\1.5.7 --add-modules javafx.controls,javafx.fxml -jar FarmDashboard\JarLauncher\FarmDashboard.jar
